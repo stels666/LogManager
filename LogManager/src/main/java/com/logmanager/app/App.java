156b7a1623201cdff9cde9b0e5ac71c19f810d33
andrei.radzetski <svisloch.stels@gmail.com>
2013-12-13 08:29:49 -0430

    client secret

--- LogManager/src/main/java/com/logmanager/app/App.java
+++ LogManager/src/main/java/com/logmanager/app/App.java
@@ -1,13 +1,21 @@
 package com.logmanager.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+
+public class App {
+
+  private static final String PROJECT_ID = "log-manager-v1";
+  private static final String CLIENTSECRETS_LOCATION = "client_secret.json";
+
+
+  public static void main(String[] args) throws IOException {
+	  GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(new JacksonFactory(), App.class.getClassLoader().getResourceAsStream(CLIENTSECRETS_LOCATION));
+
+  
+  }
+
 }
